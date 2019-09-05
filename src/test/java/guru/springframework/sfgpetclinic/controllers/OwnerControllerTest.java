8cd0af426f4da54b52948f2936ba50485fe3a42b
czarek <karakankaczyn@gmail.com>
2019-09-05 22:45:06 +0200

    argument capture

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -5,10 +5,16 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -29,7 +35,8 @@ class OwnerControllerTest {
     @InjectMocks
     OwnerController ownerController;
 
-
+@Captor
+ArgumentCaptor<String> stringArgumentCaptor;
 
     @Test
     void processCreationFormHappyPath() {
@@ -63,4 +70,35 @@ class OwnerControllerTest {
         assertEquals(VIEWS_OWNER_CREATE_OR_UPDATE_FORM ,redirectPath);
     }
 
+    @Test
+    void processFindFormWildcardString() {
+        //given
+        Owner owner = new Owner(null,"Iks","Iksinski");
+        List<Owner> ownerList = new ArrayList<>();
+        final ArgumentCaptor<String> captor=ArgumentCaptor.forClass(String.class);
+        given(ownerService.findAllByLastNameLike(captor.capture())).willReturn(ownerList);
+
+
+        //when
+        String viewName=ownerController.processFindForm(owner,bindingResult,null);
+
+        //then
+        assertThat(captor.getValue()).isEqualToIgnoringCase("%Iksinski%");
+    }
+
+
+    @Test
+    void processFindFormWildcardStringAnnotation() {
+        //given
+        Owner owner = new Owner(null,"Iks","Iksinski");
+        List<Owner> ownerList = new ArrayList<>();
+        given(ownerService.findAllByLastNameLike(stringArgumentCaptor.capture())).willReturn(ownerList);
+
+
+        //when
+        String viewName=ownerController.processFindForm(owner,bindingResult,null);
+
+        //then
+        assertThat(stringArgumentCaptor.getValue()).isEqualToIgnoringCase("%Iksinski%");
+    }
 }
